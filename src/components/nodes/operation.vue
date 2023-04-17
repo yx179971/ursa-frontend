e34e7962623fb0931912759d1c5b31f589eb58af
yx179971 <yx179971@163.com>
2023-04-17 22:36:04 +0800

    对接图片上传

--- src/components/nodes/operation.vue
+++ src/components/nodes/operation.vue
@@ -2,7 +2,7 @@
   <a-upload
       v-model:file-list="fileList"
       name="file"
-      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      :action="uploadUrl"
       :headers="headers"
       @change="handleChange"
   >
@@ -17,12 +17,14 @@ import { message } from 'ant-design-vue';
 import { UploadOutlined } from '@ant-design/icons-vue';
 import {defineComponent, inject, ref} from 'vue';
 import type { UploadChangeParam } from 'ant-design-vue';
+import conf from '../../conf.js'
 
 export default defineComponent({
   components: {
     UploadOutlined,
   },
   setup() {
+    const uploadUrl = conf.host + '/uploadfile'
     const nodeId = inject('nodeId')
     const updateNodeData = inject('updateNodeData')
     const handleChange = (info: UploadChangeParam) => {
@@ -31,6 +33,7 @@ export default defineComponent({
       }
       if (info.file.status === 'done') {
         message.success(`${info.file.name} file uploaded successfully`);
+        console.log(info.file.response)
       } else if (info.file.status === 'error') {
         message.error(`${info.file.name} file upload failed.`);
         updateNodeData(nodeId.value, 'a', '123')
@@ -39,6 +42,7 @@ export default defineComponent({
 
     const fileList = ref([]);
     return {
+      uploadUrl,
       fileList,
       headers: {
         authorization: 'authorization-text',
---
+++ src/conf.js
@@ -0,0 +1,5 @@
+const host = "http://192.168.31.197:8000"
+
+export default {
+    host
+}
